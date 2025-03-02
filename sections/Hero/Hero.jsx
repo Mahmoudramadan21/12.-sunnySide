2ac6812d3f72a5a8685e409c1d76ac08d1de49bc
Mahmoud Ramadan <mahmoud.fci25@gmailcom>
2025-03-02 23:14:40 +0200

    Improve: Enhance accessibility by adding ARIA labels and focus styles

--- sections/Hero/Hero.jsx
+++ sections/Hero/Hero.jsx
@@ -1,25 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./Hero.css";
 import scrollIndicator from "../../images/icon-arrow-down.svg";
 
 function Hero() {
-    // Define CSS classes as variables
-    const containerClasses = `container mx-auto md:px-9 px-3 flex flex-col items-center 
-                               text-center md:gap-28 gap-16 
-                               md:py-28 py-16`;
-
-    const headingClasses = 'hero-heading uppercase font-fraunces text-white tracking-widest lg:text-7xl md:text-5xl text-4xl';
-
-    const indicatorClasses = 'hero-scroll-indicator';
-
     return (
-        <section className="hero">
-            <div className={containerClasses}>
-                <h1 className={headingClasses}>
-                    We are creatives
-                </h1>
-                <div className={indicatorClasses}>
-                    <img src={scrollIndicator} alt="Scroll down indicator" />
+        <section className="hero" role="banner">
+            <div className="hero-container">
+                <h1 className="hero-heading">We are creatives</h1>
+                <div className="hero-scroll-indicator" aria-hidden="true">
+                    <img src={scrollIndicator} alt="" aria-hidden="true" />
                 </div>
             </div>
         </section>
--- sections/Projects/Projects.jsx
+++ sections/Projects/Projects.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import "./Projects.css"
-import imgOne from "../../images/desktop/image-gallery-milkbottles.jpg"
-import imgTwo from "../../images/desktop/image-gallery-orange.jpg"
-import imgThree from "../../images/desktop/image-gallery-cone.jpg"
-import imgFour from "../../images/desktop/image-gallery-sugarcubes.jpg"
-import imgOneForMobile from "../../images/desktop/image-gallery-milkbottles.jpg"
-import imgTwoForMobile from "../../images/desktop/image-gallery-orange.jpg"
-import imgThreeForMobile from "../../images/desktop/image-gallery-cone.jpg"
-import imgFourForMobile from "../../images/desktop/image-gallery-sugarcubes.jpg"
+import React from 'react';
+import "./Projects.css";
+import imgOne from "../../images/desktop/image-gallery-milkbottles.jpg";
+import imgTwo from "../../images/desktop/image-gallery-orange.jpg";
+import imgThree from "../../images/desktop/image-gallery-cone.jpg";
+import imgFour from "../../images/desktop/image-gallery-sugarcubes.jpg";
+import imgOneForMobile from "../../images/desktop/image-gallery-milkbottles.jpg";
+import imgTwoForMobile from "../../images/desktop/image-gallery-orange.jpg";
+import imgThreeForMobile from "../../images/desktop/image-gallery-cone.jpg";
+import imgFourForMobile from "../../images/desktop/image-gallery-sugarcubes.jpg";
 
 function Projects() {
     return (
-        <section id='projects' className="Projects grid grid-cols-2 md:grid-cols-4 mt-36">
-            <picture>
-                <source srcSet={imgOneForMobile} media='(max-width: 480px)' />
-                <img src={imgOne} alt="Project Item" className="project-image" />
+        <section id="projects" className="projects" aria-labelledby="projects-heading">
+            <h2 id="projects-heading" className="sr-only">Our Projects</h2>
+            <picture tabIndex="0">
+                <source srcSet={imgOneForMobile} media="(max-width: 480px)" />
+                <img src={imgOne} alt="Milk bottles on a blue background" className="project-image" loading='lazy' />
             </picture>
-            <picture>
-                <source srcSet={imgTwoForMobile} media='(max-width: 480px)' />
-                <img src={imgTwo} alt="Project Item" className="project-image" />
+            <picture tabIndex="0">
+                <source srcSet={imgTwoForMobile} media="(max-width: 480px)" />
+                <img src={imgTwo} alt="Orange slice on a plate" className="project-image" loading='lazy' />
             </picture>
-            <picture>
-                <source srcSet={imgThreeForMobile} media='(max-width: 480px)' />
-                <img src={imgThree} alt="Project Item" className="project-image" />
+            <picture tabIndex="0">
+                <source srcSet={imgThreeForMobile} media="(max-width: 480px)" />
+                <img src={imgThree} alt="Ice cream cone on a table" className="project-image" loading='lazy' />
             </picture>
-            <picture>
-                <source srcSet={imgFourForMobile} media='(max-width: 480px)' />
-                <img src={imgFour} alt="Project Item" className="project-image" />
+            <picture tabIndex="0">
+                <source srcSet={imgFourForMobile} media="(max-width: 480px)" />
+                <img src={imgFour} alt="Sugar cubes stacked on a table" className="project-image" loading='lazy' />
             </picture>
         </section>
-    )
+    );
 }
 
 export default Projects;
