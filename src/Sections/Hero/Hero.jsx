f497b5ff383c32ccb4851fc93f654886df68c11e
Mahmoud Ramadan <mahmoud.fci25@gmailcom>
2025-03-03 00:23:33 +0200

    Improve: Enhance accessibility by adding ARIA labels and focus styles

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -3,18 +3,6 @@ import "./Hero.css";
 import scrollIndicator from "../../images/icon-arrow-down.svg";
 
 function Hero() {
-<<<<<<< HEAD
-    // Define CSS classes as variables
-    const containerClasses = `container mx-auto md:px-9 px-3 flex flex-col items-center 
-                               text-center md:gap-28 gap-24
-                               md:py-28 py-16`;
-
-    const headingClasses = 'hero-heading uppercase font-fraunces text-white tracking-widest lg:text-7xl md:text-5xl text-4xl';
-
-    const indicatorClasses = 'hero-scroll-indicator';
-
-=======
->>>>>>> 179a13a (Improve: Enhance accessibility by adding ARIA labels and focus styles)
     return (
         <section className="hero" role="banner">
             <div className="hero-container">
